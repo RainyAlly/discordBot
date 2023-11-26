9668f26238bcbe50cb7dec5baf8cf85840916c42
JordanVL1234 <96630742+JordanVL1234@users.noreply.github.com>
2023-11-25 22:04:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import discord
 from discord.ext import commands
 
-BotName = "Discord Bot"
+CommandList = [
+    "$help - Opens up a command list",
+    "$hello - Introduces the bot",
+    "$test - A *small* test to see if common text features work"
+]
 
 client = commands.Bot(command_prefix = '$', intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
     print("\n\n\n\n")
-    print(f"{BotName} is now online")
+    print("GameJot is now online")
     print("\n\n\n\n")
 
 @client.command()
-async def hello(ctx):
-    await ctx.send(f"Hello, I am {BotName}")
+async def help1(ctx):
+    num = 1
+    await ctx.send("Here's my commands")
+    for i in CommandList:
+        await ctx.send(f"{num}) {CommandList[num - 1]}")
+        num = num + 1
 
-client.run('API KEY NOT HERE')
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello, I am GameJot")
+
+@client.command()
+async def ping(ctx):
+    await ctx.send("1) \n# *Text*")
+
+client.run('API KEY HERE')
